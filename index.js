5bd6e724e476624351797c286fca52ea14700890
Shubhankar02 <shborade@gmail.com>
2018-10-11 00:32:12 +0530

    add payload handler

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 
 // Server should respond to all the queries
@@ -26,11 +27,24 @@ const server = http.createServer((req,res)=>{
     // Get the headers as an object
     const headers = req.headers;
 
-    // Send the reponse
-    res.end('Hello World\n');
+    // Get the payload, if there is any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data)=>{
+        buffer += decoder.write(data);
+    });
 
-    // log the request path
-    console.log('Request received with this headers: ', headers);
+    req.on('end', () =>{
+        buffer += decoder.end();
+
+        // Send the reponse
+        res.end('Hello World\n');
+
+        // log the request path
+        console.log('Request received with this payload: ', buffer);
+        
+    });
+
 });
 
 // Start the server on port 3000
